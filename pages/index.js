e47366ab06980b7a6b534fa003119366d2c4f652
CRYPTONICJOE <110938868+CRYPTONICJOE@users.noreply.github.com>
2023-03-22 10:07:35 +0000

    update

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="DeadBotz Minting Dapp!" />
+        <Header title="DeadBotz Minting!" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
